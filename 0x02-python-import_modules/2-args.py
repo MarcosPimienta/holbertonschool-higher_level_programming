812cf631c91df5615cb0aaba6b500dea9dc87e1e
MarcosPimienta <1676@holbertonschool.com>
2020-09-03 23:38:06 +0000

    fixed colon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{:d} {}".format(len(sys.argv) - 1, "arguments."))
     elif len(sys.argv) == 2:
-        print("{:d} {}".format(len(sys.argv) - 1, "argument."))
+        print("{:d} {}".format(len(sys.argv) - 1, "argument:"))
     else:
         print("{:d} {}".format(len(sys.argv) - 1, "arguments: "))
     for i in range(1, len(sys.argv)):
