49e31ada5207155372ae20f5a7d04ae99f026aad
MarcosPimienta <1676@holbertonschool.com>
2020-09-03 23:58:46 +0000

    fixed sys call

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) == 1:
-        print("{} {}".format(len(sys.argv) - 1, "arguments."))
+        print("{} {}".format((len(sys.argv) - 1), "arguments."))
     elif len(sys.argv) == 2:
-        print("{} {}".format(len(sys.argv) - 1, "argument:"))
+        print("{} {}".format((len(sys.argv) - 1), "argument:"))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} {}".format(len(sys.argv) - 1, "arguments: "))
+        print("{} {}".format((len(sys.argv) - 1), "arguments: "))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
