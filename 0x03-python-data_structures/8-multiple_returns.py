dfb67ddea5734a49cf21f167e0fe8ee8fa37330e
MarcosPimienta <1676@holbertonschool.com>
2020-09-09 04:13:14 +0000

    fixed Null case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == None:
+    if len(sentence) == 0:
         sentence[0] = None
     newtuple = (len(sentence), sentence[0])
     return newtuple
