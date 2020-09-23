b92a008d2abbdbf0a630db9b70f86331769c5ad3
MarcosPimienta <1676@holbertonschool.com>
2020-09-23 01:30:37 +0000

    private init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class with private size"""
+
+
+class Square:
+    """Square class with private size"""
+    def __init__(self, size=None):
+        self.__size = size
+    pass  # An empty block
