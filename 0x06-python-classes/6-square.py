5bd470b71cecaacc5650e75d9357ff1a4b53bee2
MarcosPimienta <1676@holbertonschool.com>
2020-09-24 04:25:26 +0000

    fixed pep

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Square class with private instance"""
+
+
 class Square:
     """Square class with private instance"""
+
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
@@ -11,11 +14,14 @@ class Square:
             raise ValueError("size must be >= 0")
         if len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         return (self.__size ** 2)
+
     @property
     def size(self):
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -23,14 +29,17 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     @property
     def position(self):
         return (self.__position)
+
     @position.setter
     def position(self, value):
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         return ()
+
     def my_print(self):
         # times = self.__size * "#"
         print("\n" * self.__position[1], end='')
