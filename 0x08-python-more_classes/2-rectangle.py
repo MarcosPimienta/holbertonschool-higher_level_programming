b224c595f7334885385bc6f5ba02484c210e97b6
MarcosPimienta <1676@holbertonschool.com>
2020-09-29 03:44:13 +0000

    fixed output

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,9 +11,11 @@ class Rectangle:
         self.height = height
 
     def area(self):
-        return ((self.__width) * (self.__height))
+        return (self.__width * self.__height)
 
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     @property
