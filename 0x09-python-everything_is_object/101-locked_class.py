1e70d925b0384de32b3b93cd7e835e89b344aa44
MarcosPimienta <1676@holbertonschool.com>
2020-10-01 04:48:03 +0000

    pep8 fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Learning __slots__"""
+
+
 class LockedClass:
-"""LockedClass is for making sort of exception"""
+    """LockedClass is for making sort of exception"""
     __slots__ = ['first_name']
