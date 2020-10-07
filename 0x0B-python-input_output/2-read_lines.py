11e8562471a452ef7b12caa829d40dbc3f1e6c7c
MarcosPimienta <1676@holbertonschool.com>
2020-10-07 23:53:26 +0000

    read lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Read lines of file"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Read lines of file"""
+
+    with open(filename, encoding="UTF-8") as file:
+        i = 0
+        while (True):
+            num = file.readline()
+            if not num:
+                break
+            if i < nb_lines or nb_lines <= 0:
+                print (num[:-1])
+            i += 1
