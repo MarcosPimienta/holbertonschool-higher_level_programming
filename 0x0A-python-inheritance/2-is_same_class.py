f808fe068ad7c77ed488f8688bbf24880f7375b9
MarcosPimienta <1676@holbertonschool.com>
2020-10-07 00:15:49 +0000

    fixed pep8 comments

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Returns true if its the same class"""
 
 
 def is_same_class(obj, a_class):
+    """Returns true if its the same class"""
+
     if type(obj) is a_class:
         return (True)
     else:
