1a4b858eaeb69075216cc6d0118f28b858eb337a
MarcosPimienta <1676@holbertonschool.com>
2020-10-08 02:26:36 +0000

    save to JSON

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -6,5 +6,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Go for it JSON!!!!"""
+
     with open(filename, mode='w', encoding="UTF-8") as file:
-        return json.dump(my_obj, file)
+        json.dump(my_obj, file)
