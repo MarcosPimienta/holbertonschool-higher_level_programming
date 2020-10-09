dfc618a944249cf92e6c0bde5e0e4c1a8ed8711e
MarcosPimienta <1676@holbertonschool.com>
2020-10-09 02:08:57 +0000

    fixed pep8

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""Dictionary"""
+"""Program that define a Student with filter"""
 
-import attr
 
-@attr.s
-class Student(object):
-    def to_json(self):
-        return (self.__dict__)
+class Student:
+    """Class Student that defines a student"""
 
     def __init__(self, first_name, last_name, age):
+        """Constructor"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+
+        dic = {}
+        if (type(attrs) is not list):
+            return (self.__dict__)
+        else:
+            for x in attrs:
+                if (x in self.__dict__):
+                    dic[x] = self.__dict__[x]
+            return (dic)
