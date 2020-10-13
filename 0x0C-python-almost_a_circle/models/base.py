5afff566daf29b9c6775e6b21c9ecebf9b344ce9
MarcosPimienta <1676@holbertonschool.com>
2020-10-13 20:04:47 +0000

    fixed pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,14 +11,28 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Doc for constructor"""clear
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
+        """Doc for constructor"""
         if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Doc for constructor"""
+        if list_objs is None:
+            jstr = "[]"
+        else:
+            jstr = cls.to_json_string([i.to_dictionary() for i in list_objs])
+        filename = cls.__name__ + ".json"
+        with open(filename, mode='w', encoding="UTF-8") as file:
+            file.write(jstr)
