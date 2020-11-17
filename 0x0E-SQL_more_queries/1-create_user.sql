d160361664fc6abafaf0207a8d7134c71c16e536
MarcosPimienta <1676@holbertonschool.com>
2020-11-17 20:33:08 +0000

    if not exists added

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates the MySQL server user user_0d_1
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 -- Grant all priviliges to user user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
