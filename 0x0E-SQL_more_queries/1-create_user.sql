b02ce6f037133e32fbdfe9246c37d6e55752967f
MarcosPimienta <1676@holbertonschool.com>
2020-11-17 20:49:13 +0000

    password fixed

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_password';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all priviliges to user user_0d_1.
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
