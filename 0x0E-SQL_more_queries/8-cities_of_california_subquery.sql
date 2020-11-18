a5f839f7ab27fdf349c00db06ae2724457f58ed9
MarcosPimienta <1676@holbertonschool.com>
2020-11-18 19:15:25 +0000

    fields fixed

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --  lists all the cities of California that can be found in the database.
-SELECT * FROM cities WHERE state_id = 1 ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC;
