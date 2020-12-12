8914ed5f01450c095be855b9913f2e64d2c6217c
MarcosPimienta <1676@holbertonschool.com>
2020-12-12 23:32:16 +0000

    Lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-__import__("MySQLdb")
+"""Lists all states"""
 
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host="localhost", user="{}".format(argv[1]),
+                     passwd="{}".format(argv[2]),
+                     db="{}".format(argv[3]), port=3306)
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states ORDER BY id;")
+selects = cur.fetchall()
+for i in selects:
+    print("{}".format(i))
+cur.close()
+db.close()
