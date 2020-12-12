39afafc4631a3ccc1f60d03336d049ed11842dce
MarcosPimienta <1676@holbertonschool.com>
2020-12-12 23:41:35 +0000

    No exec when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,15 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", user="{}".format(argv[1]),
-                     passwd="{}".format(argv[2]),
-                     db="{}".format(argv[3]), port=3306)
-cur = db.cursor()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user="{}".format(argv[1]),
+                         passwd="{}".format(argv[2]),
+                         db="{}".format(argv[3]), port=3306)
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY id;")
-selects = cur.fetchall()
-for i in selects:
-    print("{}".format(i))
-cur.close()
-db.close()
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    selects = cur.fetchall()
+    for i in selects:
+        print("{}".format(i))
+    cur.close()
+    db.close()
