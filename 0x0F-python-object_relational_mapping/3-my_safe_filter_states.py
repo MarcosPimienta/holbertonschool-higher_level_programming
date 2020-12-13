ce45da0a4772dfe80dbc1dc0e12065430fe84021
MarcosPimienta <1676@holbertonschool.com>
2020-12-13 02:55:50 +0000

    sql inj

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists all states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user="{}".format(argv[1]),
+                         passwd="{}".format(argv[2]),
+                         db="{}".format(argv[3]), port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'\
+                 ORDER BY id".format(argv[4]))
+    selects = cur.fetchall()
+    for i in selects:
+        print("{}".format(i))
+    cur.close()
+    db.close()
