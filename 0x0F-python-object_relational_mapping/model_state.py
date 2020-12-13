7abf4a4f7b6d9b433b401e53c68f2c6ebf631449
MarcosPimienta <1676@holbertonschool.com>
2020-12-13 21:41:22 +0000

    sqlalchemy no unique

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,6 +6,6 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, unique=True,
+    id = Column(Integer, autoincrement=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
