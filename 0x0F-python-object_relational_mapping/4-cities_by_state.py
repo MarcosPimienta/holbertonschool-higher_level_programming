8b678be5f124586701023ed77d574264a783ee31
MarcosPimienta <1676@holbertonschool.com>
2020-12-13 17:44:44 +0000

    pep8 fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
                          db="{}".format(argv[3]), port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities INNER JOIN states ON cities.state_id = states.id")
     selects = cur.fetchall()
     for i in selects:
         print("{}".format(i))
