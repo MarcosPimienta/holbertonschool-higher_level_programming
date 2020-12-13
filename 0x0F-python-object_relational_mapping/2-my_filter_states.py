69b5041d06707420f492e2052d2da82c4afd00ac
MarcosPimienta <1676@holbertonschool.com>
2020-12-13 00:44:57 +0000

    Lists all states from user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists all states from user input"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user="{}".format(argv[1]),
+                         passwd="{}".format(argv[2]),
+                         db="{}".format(argv[3]), port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
+    selects = cur.fetchall()
+    for i in selects:
+        print("{}".format(i))
+    cur.close()
+    db.close()
