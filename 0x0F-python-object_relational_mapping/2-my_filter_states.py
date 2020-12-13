97922ec093254321ed1e68db78873f3af2a29cff
MarcosPimienta <1676@holbertonschool.com>
2020-12-13 00:56:26 +0000

    fixed format by ID

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states from user input"""
+"""Lists all states"""
 
 import MySQLdb
 from sys import argv
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          db="{}".format(argv[3]), port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name = '{:%s}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = '{}'ORDER BY id".format(argv[4]))
     selects = cur.fetchall()
     for i in selects:
         print("{}".format(i))
