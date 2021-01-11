0b97ce6e2ecbd3693556e88d935c58eb21057b13
MarcosPimienta <1676@holbertonschool.com>
2021-01-11 17:46:12 +0000

    fixed import exec and ''

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Fetch status from web"""
 from urllib import request
-with request.urlopen("https://intranet.hbtn.io/status") as resp:
-    data = resp.read()
-    html = data.decode("UTF-8")
 
-print("{}\n\t- type: {}\n\t- content: {}\n\t- utf8 content: '{}'"
-      .format("Body response:", type(data), data, html))
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as resp:
+        data = resp.read()
+        html = data.decode("UTF-8")
+
+    print("{}\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+        .format("Body response:", type(data), data, html))
