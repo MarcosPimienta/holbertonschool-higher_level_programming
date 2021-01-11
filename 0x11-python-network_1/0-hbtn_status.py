eea416a61152e835cb1d0e5b135ec46a33e64e00
MarcosPimienta <1676@holbertonschool.com>
2021-01-11 17:42:56 +0000

    fixed utf-8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 from urllib import request
 with request.urlopen("https://intranet.hbtn.io/status") as resp:
     data = resp.read()
-    html = data.decode()
+    html = data.decode("UTF-8")
 
 print("{}\n\t- type: {}\n\t- content: {}\n\t- utf8 content: '{}'"
       .format("Body response:", type(data), data, html))
