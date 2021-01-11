3740f924860101672031d2badbeaf1e855e1e3ca
MarcosPimienta <1676@holbertonschool.com>
2021-01-11 17:37:05 +0000

    fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """Fetch status from web"""
 from urllib import request
-resp = request.urlopen("https://intranet.hbtn.io/status")
-data = resp.read()
-code = resp.code
-html = data.decode("UTF-8")
-print("- type: {}".format(type(data)))
-if code == 200:
-    print("- content: b'{}'".format("OK"))
-if type(html) == str:
-    print("- utf8 content:'{}'".format("OK"))
+with request.urlopen("https://intranet.hbtn.io/status") as resp:
+    data = resp.read()
+    html = data.decode()
+
+print("{}\n- type: {}\n- content: {}\n- utf8 content: '{}'"
+      .format("Body response:", type(data), data, html))
