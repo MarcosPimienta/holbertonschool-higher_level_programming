6b5e922706f3d45b09089e647116290294550d05
MarcosPimienta <1676@holbertonschool.com>
2021-01-12 03:54:36 +0000

    print format fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     elif len(jsonRes) == 0:
         print("No result")
     else:
-        jstring = jsonRes.get('id')
+        jstring = jsonRes.get('[id]')
         print(jstring, jsonRes['name'], sep=' ')
