fc5e356bfa6e435abb8244c4c2cdaa97d0e800c7
MarcosPimienta <1676@holbertonschool.com>
2021-02-02 21:32:38 -0500

    square repeat

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ const Num = parseInt(process.argv[2]);
 if (!Num) {
   console.log('Missing size');
 }
-for (let i = 0; i < num; i++) {
-  console.log('X'.repeat(num));
+for (let i = 0; i < Num; i++) {
+  console.log('X'.repeat(Num));
 }
