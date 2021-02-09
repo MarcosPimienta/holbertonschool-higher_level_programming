912aa4bc923bcadab1774713b69640a758172d03
MarcosPimienta <1676@holbertonschool.com>
2021-02-09 16:11:07 -0500

    fixed semi

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 request(process.argv[2], function (error, response) {
-    if (process.argv[2] === undefined){
-        console.error('error:', error);
-    }
+  if (process.argv[2] === undefined) {
+    console.error('error:', error);
+  }
   console.log('code:', response.statusCode);
 });
