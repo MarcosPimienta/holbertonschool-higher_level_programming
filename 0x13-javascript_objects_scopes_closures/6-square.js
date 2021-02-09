7f0bf07835ba88c7a1537d2911c72b3703a7f60a
MarcosPimienta <1676@holbertonschool.com>
2021-02-08 20:21:42 -0500

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const SquareX = require('./5-square');
+
+class Square extends SquareX {
+
+  charPrint (C) {
+    if (C == null){
+      C = 'X';
+    }
+    let i = 0;
+    for (i; i < this.height; i++) {
+      console.log(C.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
