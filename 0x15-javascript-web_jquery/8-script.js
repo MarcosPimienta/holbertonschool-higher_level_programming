44528514d9764685ef747ec95407e250493edbc8
MarcosPimienta <1676@holbertonschool.com>
2021-02-11 23:05:50 -0500

    done at last

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+    $.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+      for (const i of data.results) {
+        $('UL#list_movies').append('<li>' + i.title + '</li>');
+      }
+    });
+  });
